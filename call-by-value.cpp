532cf0bfdcdfda06531fd2653810078d389c9358
deepaksyadav <deepaksyadav@users.noreply.github.com>
2022-12-01 14:00:03 +0530

    Create call-by-value.cpp

---
+++ call-by-value.cpp
@@ -0,0 +1,19 @@
+// C++ program to demonstrate call by value 
+
+#include<iostream>  
+using namespace std;    
+void swap(int x, int y)  
+{  
+ int swap;  
+ swap=x;  
+ x=y;  
+ y=swap;  
+}  
+int main()   
+{    
+ int x=218, y=812;    
+ swap(x, y);  // passing value to function  
+ cout<<"Value of x is: "<<x<<endl;  
+ cout<<"Value of y is: "<<y<<endl;  
+ return 0;  
+}  
